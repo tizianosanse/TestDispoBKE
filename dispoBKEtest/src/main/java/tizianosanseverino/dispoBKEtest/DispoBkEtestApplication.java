ba78b82c6c79ee972eac7b0461b349fc1ce7d884
tizianosanse <157717522+tizianosanse@users.noreply.github.com>
2024-06-28 16:00:24 +0200

    add project

---
+++ dispoBKEtest/src/main/java/tizianosanseverino/dispoBKEtest/DispoBkEtestApplication.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.dispoBKEtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispoBkEtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispoBkEtestApplication.class, args);
+	}
+
+}
---
+++ dispoBKEtest/src/test/java/tizianosanseverino/dispoBKEtest/DispoBkEtestApplicationTests.java
@@ -0,0 +1,13 @@
+package tizianosanseverino.dispoBKEtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispoBkEtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
